540b4543575fea13a7ac44f8e7107cf1dd07bb92
Jose <josemchapela@gmail.com>
2024-02-16 00:37:26 +0100

    Initial commit Creada la pantalla de nivel Separado el juego en clases para poder añadir más pantallas Trabajados ViewPorts para la relación de aspecto en distintos tamaños de pantalla Creación de un HUD para mostrar información en un nivel Creado un mapa básico con Tiled para probar las colisiones y físicas Creado un mundo de físicas gracias a Box2D y creadas las figuras/formas/objetos que corresponden al suelo del mapa Creado un personaje de prueba para comprobar que se le aplica la gravedad y que colisiona con el suelo creado anteriormente Extracción de varios métodos comunes a clases para una mejor orientación a objetos
    Signed-off-by: Jose <josemchapela@gmail.com>

--- core/src/com/josema/alienelysium2d/MyGdxGame.java
+++ core/src/com/josema/alienelysium2d/MyGdxGame.java
@@ -10,7 +10,7 @@ import com.josema.alienelysium2d.screens.PlayScreen;
 public class MyGdxGame extends Game {
 	public static final int V_WIDTH=400;
 	public static final int V_HEIGHT=208;
-	public static final float PPM=100;
+	public static final float PPM=90;
 
 	public SpriteBatch batch;
 	
